1129fce8fcd4bd743ebd40bf3c94ccca5a5a9899
veeresh <veereshediga111@gamil.com>
2023-03-29 10:36:33 +0530

    solution

--- data-project/src/App.js
+++ data-project/src/App.js
@@ -1,19 +1,16 @@
+import {Switch, Route} from 'react-router-dom';
 
-import { Component } from "react";
-import Login from "./Components/Login";
-import Register from "./Components/Register";
+import Login from './Components/Login';
+import Register from './Components/Register'
+import Verify from './Components/Verify';
+import "./App.css"
 
-
-class App extends Component{
-  render(){
-    return(
-      <>
-        <Login />
-        <Register />
-      </>
-    )
-  }
-}
+const App = () =>(
+  <Switch>
+    <Route exact path="/" Component={Login}/>
+    <Route exact path= "/verify" component={Verify}/>
+    <Route exact path='/register' Component={Register}/>
+  </Switch>
+)
 
 export default App
-  
